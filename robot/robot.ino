a5fcfc87e940917408e7a4de45276de63f57c7f5
chiva <santireig@gmail.com>
2012-02-20 11:25:53 +0100

    iniciado el codigo principal del robot

---
+++ robot/robot.ino
@@ -0,0 +1,48 @@
+#include <Motor298.h>
+#include <Driver298.h>
+#include <Bluetooth.h>
+
+/**********************/
+/* Definiciones Motor */
+/**********************/
+// No usar PWM 5 y 6, no llegan a 0
+#define MT_IZQ_AVANCE   3
+#define MT_IZQ_RETRO    4
+#define MT_IZQ_CONTROL  5
+#define MT_DCHA_AVANCE  6
+#define MT_DCHA_RETRO   7
+#define MT_DCHA_CONTROL 6
+
+#if !digitalPinHasPWM(MT_IZQ_CONTROL) || !digitalPinHasPWM(MT_DCHA_CONTROL)
+  #error "Los pines de control deben ser PWM"
+#endif
+
+Driver298 move(MT_IZQ_AVANCE, MT_IZQ_RETRO, MT_IZQ_CONTROL,
+               MT_DCHA_AVANCE, MT_DCHA_RETRO, MT_DCHA_CONTROL);
+
+/**************************/
+/* Definiciones Bluetooth */
+/**************************/
+#define BT_RESET 10
+#define BT_NAME "ROBOTO"
+#define BT_PINCODE "1234"
+
+Bluetooth bt(BT_RESET);
+
+/*****************************/
+/* Definiciones Sensor Línea */
+/*****************************/
+
+
+void setup(){
+  Serial.begin(9600);
+  
+  #ifdef BT_NAME
+    bt.setName(BT_NAME);
+  #endif
+  #ifdef BT_PINCODE
+    bt.setPinCode(BT_PINCODE);
+  #endif
+}
+
+void loop(){}
