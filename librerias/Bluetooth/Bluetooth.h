d0d268ae8e16b98a5d5f76702d6e4be465d71af1
chiva <santireig@gmail.com>
2012-02-20 11:25:21 +0100

    añadida libreria para Bluetooth

---
+++ librerias/Bluetooth/Bluetooth.h
@@ -0,0 +1,71 @@
+#ifndef Bluetooth_h
+#define Bluetooth_h
+
+#include "Arduino.h"
+
+class Bluetooth
+{
+  private:
+    byte pinReset;
+    boolean connected();
+    void dumpResponse();
+  public:
+    Bluetooth(byte);
+    void turnOn();
+    void turnOff();
+    void setPinCode(char[]);
+    void setName(char[]);
+    void reset();
+};
+
+Bluetooth::Bluetooth(byte pinReset){
+    this->pinReset = pinReset;
+    pinMode(pinReset, OUTPUT);
+    turnOff();
+}
+
+boolean Bluetooth::connected(){
+    return millis() > 1000;
+}
+
+void Bluetooth::dumpResponse(){
+    unsigned long inicial = millis();
+    
+    while (!Serial.available());
+    while (millis()-inicial < 250){
+        Serial.read();
+    }
+}
+
+void Bluetooth::setPinCode(char pin[]){
+    if (!connected()){
+        Serial.print("AT+PIN");
+        Serial.print(pin);
+        dumpResponse();
+    }
+}
+
+void Bluetooth::setName(char name[]){
+    if (!connected()){
+        Serial.print("AT+NAME");
+        Serial.print(name);
+        dumpResponse();
+    }
+}
+
+inline void Bluetooth::turnOn(){
+    digitalWrite(pinReset, HIGH);
+}
+
+inline void Bluetooth::turnOff(){
+    digitalWrite(pinReset, LOW);
+}
+
+void Bluetooth::reset(){
+    if (digitalRead(pinReset)){
+        turnOff();
+        turnOn();
+    }
+}
+
+#endif
