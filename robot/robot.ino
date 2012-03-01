d64ab799c44c10c7d8f0e9b67a281fc8cdfc94c7
chiva <santireig@gmail.com>
2012-03-01 11:22:31 +0100

    robot: añadido codigo basico de seguimiento de linea

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,6 +1,7 @@
 #include <Motor298.h>
 #include <Driver298.h>
 #include <Bluetooth.h>
+#include <LineSensor.h>
 
 /**********************/
 /* Definiciones Motor */
@@ -12,27 +13,30 @@
 #define MT_DCHA_AVANCE  6
 #define MT_DCHA_RETRO   7
 #define MT_DCHA_CONTROL 6
+#define MT_MAX_PWM      1024
 
 #if !digitalPinHasPWM(MT_IZQ_CONTROL) || !digitalPinHasPWM(MT_DCHA_CONTROL)
   #error "Los pines de control deben ser PWM"
 #endif
 
-Driver298 move(MT_IZQ_AVANCE, MT_IZQ_RETRO, MT_IZQ_CONTROL,
-               MT_DCHA_AVANCE, MT_DCHA_RETRO, MT_DCHA_CONTROL);
+Driver298 motor(MT_IZQ_AVANCE, MT_IZQ_RETRO, MT_IZQ_CONTROL,
+                MT_DCHA_AVANCE, MT_DCHA_RETRO, MT_DCHA_CONTROL);
 
 /**************************/
 /* Definiciones Bluetooth */
 /**************************/
-#define BT_RESET 10
-#define BT_NAME "ROBOTO"
-#define BT_PINCODE "1234"
+#define BT_RESET    10
+#define BT_NAME     "ROBOTO"
+#define BT_PINCODE  "1234"
 
 Bluetooth bt(BT_RESET);
 
 /*****************************/
 /* Definiciones Sensor Línea */
 /*****************************/
-
+byte pinLine[LN_SENSORS] = {1,2,3,4,5,6};
+LineSensor line(pinLine);
+byte ratio;
 
 void setup(){
   Serial.begin(9600);
@@ -49,4 +53,17 @@ void setup(){
   #endif
 }
 
-void loop(){}
+void loop(){
+  #ifdef LN_CONTROLLER_K
+  ratio = line.follow();
+  if (ratio > 0){
+    motor.setSpeed(D298_FW, MT_MAX_PWM, D298_FW, map(ratio,0,5,MT_MAX_PWM,256));
+  }
+  else if (ratio < 0){
+    motor.setSpeed(D298_FW, map(ratio,0,-5,MT_MAX_PWM,256), D298_FW, MT_MAX_PWM);
+  }
+  else{
+    motor.setSpeed(D298_FW, MT_MAX_PWM);
+  }
+  #endif
+}
