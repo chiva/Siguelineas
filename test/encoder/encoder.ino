215a918edf6c236bd0aa3320ac6697dc90263d62
chiva <santireig@gmail.com>
2012-04-24 16:22:15 +0200

    test: actualizacion de estado inmediata en encoder y linea

--- test/encoder/encoder.ino
+++ test/encoder/encoder.ino
@@ -3,14 +3,14 @@ byte sensores[] = {3,2}; //I,D
 
 void setup(){
   Serial.begin(9600);
-  for (int i=0; i<(sizeof(sensores)/sizeof(byte)); i++){
+  for (int i=0; i<sizeof(sensores)); ++i){
     pinMode(sensores[i], INPUT);
   }
+  Serial.println("Lectura encoder: ");
 }
 
 void loop(){
-  Serial.println("Lectura encoder: ");
-  for (int i=0; i<(sizeof(sensores)/sizeof(byte)); i++){
+  for (int i=0; i<(sizeof(sensores)); ++i){
     Serial.print(digitalRead(sensores[i]));
     Serial.print(" ");
   }
--- test/linea/linea.ino
+++ test/linea/linea.ino
@@ -3,17 +3,16 @@ byte sensores[] = {7,8,A4,4,5,6}; //D3,D2,D1,I1,I2,I3
 
 void setup(){
   Serial.begin(9600);
-  for (int i=0; i<(sizeof(sensores)/sizeof(byte)); i++){
+  for (int i=0; i<(sizeof(sensores)); i++){
     pinMode(sensores[i], INPUT);
   }
+  Serial.println("Lectura sensores: ");
 }
 
 void loop(){
-  Serial.println("Lectura sensores: ");
-  for (int i=0; i<(sizeof(sensores)/sizeof(byte)); i++){
+  for (int i=0; i<(sizeof(sensores)); i++){
     Serial.print(digitalRead(sensores[i]));
     Serial.print(" ");
   }
-  Serial.println();
-  delay(500);
+  Serial.print('\r');
 }
