3a30a09d0313cc6626b4be5292be59859b1a4dfd
chiva <santireig@gmail.com>
2012-05-08 19:03:55 +0200

    linea: arreglado seguimiento

--- librerias/LineSensor/LineSensor.h
+++ librerias/LineSensor/LineSensor.h
@@ -6,7 +6,7 @@
 #include <Driver298.h>
 
 #define LN_SENSORS 6
-#define LN_CONTROLLER_K true
+#define LN_CONTROLLER_K
 
 class LineSensor
 {
@@ -15,9 +15,8 @@ class LineSensor
     byte pinSensor[LN_SENSORS];    // {IZQ_3, IZQ_2, IZQ_1, DCHA_1, DCHA_2, DCHA_3}
     byte lastRead[LN_SENSORS];
     static const int8_t sensorWeight[LN_SENSORS];
-    byte weight, lastWeight;
-    byte sensorsOn;
     void correct(int8_t);
+    void junctionMove();
   public:
     LineSensor(Driver298*, byte[]);
     void read();
@@ -25,9 +24,7 @@ class LineSensor
 };
 
 LineSensor::LineSensor(Driver298 *motor, byte pinSensor[]){
-    lastWeight = 0;
     this->motor = motor;
-    pinMode(LED_BUILTIN, OUTPUT);
     
     for (byte i=0; i<LN_SENSORS; i++){
         this->pinSensor[i] = pinSensor[i];
@@ -37,6 +34,12 @@ LineSensor::LineSensor(Driver298 *motor, byte pinSensor[]){
 
 const int8_t LineSensor::sensorWeight[LN_SENSORS] = {-5, -3, -1, 1, 3, 5};
 
+inline void LineSensor::junctionMove(){
+    motor->setSpeed(D298_FW, D298_FULL_SPEED, D298_BW, D298_FULL_SPEED);
+}
+
+#ifdef LN_CONTROLLER_K
+
 inline void LineSensor::read(){
     for (byte i=0; i<LN_SENSORS; i++){
         lastRead[i] = digitalRead(pinSensor[i]);
@@ -44,35 +47,37 @@ inline void LineSensor::read(){
 }
 
 void LineSensor::correct(int8_t weight){
+    Serial.print(F("Weight: "));
+    Serial.println(weight);
     if (weight > 0){
-        motor->setSpeed(D298_FW, D298_MAX_SPEED, D298_FW, map(weight,0,5,D298_MAX_SPEED,256));
+        motor->setSpeed(D298_FW, D298_FULL_SPEED, D298_FW, map(abs(weight),0,5,D298_FULL_SPEED,0));
     }
     else if (weight < 0){
-        motor->setSpeed(D298_FW, map(weight,0,-5,D298_MAX_SPEED,256), D298_FW, D298_MAX_SPEED);
+        motor->setSpeed(D298_FW, map(abs(weight),0,5,D298_FULL_SPEED,0), D298_FW, D298_FULL_SPEED);
     }
     else{
-        motor->setSpeed(D298_FW, D298_MAX_SPEED);
+        motor->forward(D298_FULL_SPEED);
     }
 }
 
-#ifdef LN_CONTROLLER_K
-
 byte LineSensor::follow(){
+    int8_t weight, lastWeight, sensorsOn;
+    
     Serial.println(F("Modo siguelineas activado"));
+    lastWeight = 0;
     while(1){
         read();
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
         weight = 0;
         sensorsOn = 0;
         for (byte i=0; i<LN_SENSORS; i++){
-            if (lastRead[i]){
+            if (!lastRead[i]){
                 weight += sensorWeight[i];
                 sensorsOn++;
             }
         }
         weight /= sensorsOn;
         if (!sensorsOn){
-            correct(lastWeight);
+            junctionMove();
         } else {
             lastWeight = weight;
             correct(weight);
@@ -80,6 +85,6 @@ byte LineSensor::follow(){
     }
 }
 
-#endif
+#endif //LN_CONTROLLER_K
 
-#endif
+#endif //LineSensor_h
