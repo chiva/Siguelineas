748347031fcf10b1d4abc4b850d6e7a585a1cece
chiva <santireig@gmail.com>
2012-05-08 17:42:21 +0200

    test: unificados test de motores

--- test/motor/motor.ino
+++ test/motor/motor.ino
@@ -1,18 +1,23 @@
 #include <Motor298.h>
 #include <Driver298.h>
 
-Driver298 motor(A0,A1,10,A2,A3,9);
+Driver298 motor(A0,A1,10,A2,A3,9), *test;
 
 void setup(){
+  test = &motor;
+  for(int i=D298_FULL_SPEED/2; i<=D298_FULL_SPEED; ++i){
+    test->forward(i);
+    delay(25);
+  }
 }
 
 void loop(){
-  for(int i=D298_FULL_SPEED/2; i<=D298_FULL_SPEED; ++i){
-    motor.forward(i);
-    delay(50);
-    if(i == D298_FULL_SPEED){
-      motor.forward(i);
-      delay(2000);
-    }
-  }
+    test->forward(D298_FULL_SPEED);
+    delay(2000);
+    test->reverse(D298_FULL_SPEED);
+    delay(2000);
+    test->turn(D298_LEFT,D298_FULL_SPEED,D298_FULL_SPEED);
+    delay(2000);
+    test->turn(D298_RIGHT,D298_FULL_SPEED,D298_FULL_SPEED);
+    delay(2000);
 }
